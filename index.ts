d70a80b5e4a46d5322ff1a29a37f9a157b9aa913
saad-rao <167574691+saad-rao@users.noreply.github.com>
2024-04-20 18:51:27 -0700

    created number guessing game

---
+++ index.ts
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from  "inquirer"
+ const randomnumber=Math.floor(Math.random() * 6+1 );
+ console.log(randomnumber);
+const answers = await inquirer.prompt([
+    {
+        name:"userguessednumber",
+        type:"number",
+        message:"please guess a number between 1-6",
+    }
+]);
+if (answers.userguessednumber === randomnumber){
+    console.log("congo!! you guessed a right number");
+} else {
+    console.log("you guessed a wrong number");
+}
+ 
